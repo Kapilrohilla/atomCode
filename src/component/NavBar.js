fd37a26455f8905add0896b74fcc1ca3a1585e41
kapilRohilla <kapilrohilla2002@gmail.com>
2023-03-10 20:19:28 +0530

    changed hamburger menu icon

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,21 +1,50 @@
 // import css
 import './navBar.css'
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 export function NavBar() {
-    let [dispayMenuIcon,setDisplayMenuIcon] = useState(true);
+    let [dispayMenuIcon,setDisplayMenuIcon] = useState(false);
+    const rotableMenuRef = useRef('');
+    const handleMenuClick = () => {
+        setDisplayMenuIcon(!dispayMenuIcon);
+        if(rotableMenuRef.current.children[0].style.rotate !== '-45deg'){
+            rotableMenuRef.current.children[0].style.rotate = '-45deg';
+            rotableMenuRef.current.children[1].style.rotate = '45deg';
+        }else{
+            rotableMenuRef.current.children[0].style.rotate = 'initial';
+            rotableMenuRef.current.children[1].style.rotate = 'initial';
+        }
+    }
+    // useEffect(()=>{
+    //     if(dispayMenuIcon === undefined){
+    //         // rotableMenuRef.current.children[0].style.rotate = '-45deg';
+    //         // rotableMenuRef.current.children[1].style.rotate = '45deg';
+    //         console.log(useRef.current);
+    //     } else if(dispayMenuIcon === true){
+    //         rotableMenuRef.current.children[0].style.rotate = '45deg';
+    //         rotableMenuRef.current.children[1].style.rotate = '-45deg';
+    //     }else if(dispayMenuIcon === false){
+    //         rotableMenuRef.current.children[0].style.rotate = '-45deg';
+    //         rotableMenuRef.current.children[1].style.rotate = '45deg';
+    //     } 
+    // },[dispayMenuIcon])
     return(
         <div id="navBar" >
             <div className="logo">
+                <img src="./images/logo.svg" alt="logo" />
                 atomcode
             </div>
-            <div className="menu" onClick={()=> setDisplayMenuIcon(!dispayMenuIcon)}>
+            {/* <div className="menu" onClick={()=> setDisplayMenuIcon(!dispayMenuIcon)}>
                 {dispayMenuIcon?
                     <p><img src="../images/menu.svg" alt="menu" /></p>:
                     <p><img src="../images/close.svg" alt="close" /></p>
                 }
+            </div> */}
+            <div ref={rotableMenuRef} className="rotableMenu" onClick={handleMenuClick}>
+                <div className='bar'></div>
+                <div className='bar'></div>
             </div>
-            <div className={dispayMenuIcon?'hide desktop-routes':'desktop-routes'}>
+            <div className={dispayMenuIcon?'desktop-routes':'hide desktop-routes'}>
                 <div>
                     <p>Features</p>
                 </div>
